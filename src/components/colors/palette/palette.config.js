cadccf6220015d70aff75025e55079d4a6f6a671
Austin Sharman <asharman22@gmail.com>
2020-08-05 16:29:31 -0400

    :sparkles: Add color palette

---
+++ src/components/colors/palette/palette.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+    context: {
+        colors: [
+            [
+                'honey-lightest',
+                'honey-lighter',
+                'honey-light',
+                'honey',
+                'honey-dark',
+                'honey-darker',
+                'honey-darkest',
+            ],
+            [
+                'gold-lightest',
+                'gold-lighter',
+                'gold-light',
+                'gold',
+                'gold-dark',
+                'gold-darker',
+                'gold-darkest',
+            ],
+            [
+                'sky-lightest',
+                'sky-lighter',
+                'sky-light',
+                'sky',
+                'sky-dark',
+                'sky-darker',
+                'sky-darkest',
+            ],
+            [
+                'wood-lightest',
+                'wood-lighter',
+                'wood-light',
+                'wood',
+                'wood-dark',
+                'wood-darker',
+                'wood-darkest',
+            ],
+        ],
+    },
+};
